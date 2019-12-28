3df86a1982843ba96b1b25ee28fa6b7972cef9c9
Skladkansaeng <59011344@kmitl.ac.th>
2019-12-28 20:07:44 +0700

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -22,30 +22,29 @@ void setup() {
   pinMode(ledPin,OUTPUT);
 }
 
-uint8_t readDiv(){
-  uint16_t swDiv =0 ;
+bool readDiv(){
+  bool swDiv = false;
   for(int8_t i=0;i<8;i++){
-      swDiv &= mcp.digitalRead(i);
-      swDiv <<= 1 ;
+      swDiv |= mcp.digitalRead(i);
   }
   #ifdef useMCP
-    swDiv &= mcp.digitalRead(swReset);
-    swDiv <<= 1;
-    swDiv &= mcp.digitalRead(swMode);
-    swDiv <<= 1;
-    swDiv &= mcp.digitalRead(swFormat);
+    swDiv |= mcp.digitalRead(swReset);
+
+    swDiv |= mcp.digitalRead(swMode);
+
+    swDiv |= mcp.digitalRead(swFormat);
   # else
-    swDiv &= digitalRead(swReset);
-    swDiv <<= 1;
-    swDiv &= digitalRead(swMode);
-    swDiv <<= 1;
-    swDiv &= digitalRead(swFormat);
+    swDiv |= digitalRead(swReset);
+
+    swDiv |= digitalRead(swMode);
+
+    swDiv |= digitalRead(swFormat);
   #endif
     return swDiv;
 }
 
 void loop() {
-  if(readDiv() != 0){
+  if(readDiv()){
     digitalWrite(ledPin,HIGH);
   }
   else{
