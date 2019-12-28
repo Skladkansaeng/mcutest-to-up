9e089d00918ca88e897c386b9604531667eaa430
Skladkansaeng <59011344@kmitl.ac.th>
2019-12-28 20:35:22 +0700

    fix:

--- src/main.cpp
+++ src/main.cpp
@@ -8,44 +8,66 @@
 // #define useMCP  // if connet all pin in mcp uncomment this code
 Adafruit_MCP23017 mcp;
 
-void setup() {
+void setup()
+{
   mcp.begin(0);
-  for(uint8 i=0;i<16;i++){
-    mcp.pinMode(i,INPUT);
+  for (uint8 i = 0; i < 16; i++)
+  {
+    mcp.pinMode(i, INPUT);
   }
   Serial.begin(9600);
-  #ifdef useMCP
-  pinMode(swReset,INPUT);
-  pinMode(swMode,INPUT);
-  pinMode(swFormat,INPUT);
-  #endif  
-  pinMode(ledPin,OUTPUT);
+#ifdef useMCP
+  pinMode(swReset, INPUT);
+  pinMode(swMode, INPUT);
+  pinMode(swFormat, INPUT);
+#endif
+  pinMode(ledPin, OUTPUT);
 }
 
-bool readDip(){
+bool readDip()
+{
   bool swDip = false;
   Serial.print("Dip switch :");
-  for(int8_t i=0;i<8;i++){
-      swDip |= mcp.digitalRead(i);
+  for (int8_t i = 0; i < 8; i++)
+  {
+    swDip |= mcp.digitalRead(i);
+    Serial.print(swDip);
+    Serial.print(" ");
   }
-  #ifdef useMCP
-    swDip |= mcp.digitalRead(swReset);
-    swDip |= mcp.digitalRead(swMode);
-    swDip |= mcp.digitalRead(swFormat);
-  # else
-    swDip |= digitalRead(swReset);
-    swDip |= digitalRead(swMode);
-    swDip |= digitalRead(swFormat);
-  #endif
-    return swDip;
+  Serial.println();
+#ifdef useMCP
+  swDip |= mcp.digitalRead(swReset);
+  Serial.print(swDip);
+  Serial.print(" ");
+  swDip |= mcp.digitalRead(swMode);
+  Serial.print(swDip);
+  Serial.print(" ");
+  swDip |= mcp.digitalRead(swFormat);
+  Serial.print(swDip);
+  Serial.print(" ");
+#else
+  swDip |= digitalRead(swReset);
+  Serial.print(swDip);
+  Serial.print(" ");
+  swDip |= digitalRead(swMode);
+  Serial.print(swDip);
+  Serial.print(" ");
+  swDip |= digitalRead(swFormat);
+  Serial.print(swDip);
+  Serial.print(" ");
+#endif
+  return swDip;
 }
 
-void loop() {
-  if(readDip()){
-    digitalWrite(ledPin,HIGH);
+void loop()
+{
+  if (readDip())
+  {
+    digitalWrite(ledPin, HIGH);
   }
-  else{
-    digitalWrite(ledPin,LOW);
+  else
+  {
+    digitalWrite(ledPin, LOW);
   }
   delay(delayLoop);
 }
